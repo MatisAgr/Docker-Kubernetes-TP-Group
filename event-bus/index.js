0a21a3c8f5418edc5b35f758e8bf089ceb659610
Tisma <matisagr@gmail.com>
2025-01-09 14:15:12 +0100

    Mettre à jour les noms des services dans l'événement bus pour assurer la cohérence des microservices et modifier la configuration de l'ingress pour utiliser des expressions régulières.

--- event-bus/index.js
+++ event-bus/index.js
@@ -12,16 +12,16 @@ app.post("/events", (req, res) => {
 
   events.push(event);
 
-  axios.post("http://posts-clusterip-srv:4000/events", event).catch((err) => {
+  axios.post("http://ms-posts-srv:4000/events", event).catch((err) => {
     console.log(err.message);
   });
-  axios.post("http://comments-srv:4001/events", event).catch((err) => {
+  axios.post("http://ms-comments-srv:4001/events", event).catch((err) => {
     console.log(err.message);
   });
-  axios.post("http://query-srv:4002/events", event).catch((err) => {
+  axios.post("http://ms-query-srv:4002/events", event).catch((err) => {
     console.log(err.message);
   });
-  axios.post("http://moderation-srv:4003/events", event).catch((err) => {
+  axios.post("http://ms-moderation-srv:4003/events", event).catch((err) => {
     console.log(err.message);
   });
   res.send({ status: "OK" });
